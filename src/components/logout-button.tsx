c214e3eb515685f2464a7e3113f9bc31b1d3d60e
Andrea Barghigiani <a.barghigiani@gmail.com>
2025-11-13 09:18:00 +0100

    chore: quality of life improvements

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -9,6 +9,7 @@ export default function LogoutButton() {
 
   return (
     <Button
+      variant="link"
       onClick={() =>
         authClient.signOut({
           fetchOptions: {
--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -1,4 +1,5 @@
 import type { User } from "better-auth";
+import { CircleUserRoundIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
@@ -7,28 +8,28 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { auth } from "@/lib/auth/auth";
 import LogoutButton from "./logout-button";
 
 export default function UserMenu({ user }: { user: User }) {
-  console.log("user:", user);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <span className="absolute -inset-1.5" />
-          <span className="relative flex h-8 w-8 items-center justify-center rounded-full">
-            <span className="h-2 w-2 rounded-full bg-secondary" />
-          </span>
+        <Button variant="ghost" size="icon" className="rounded-full">
+          <CircleUserRoundIcon className="size-6" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end">
         <DropdownMenuItem>
-          <Link href="/profile">Profile</Link>
+          <Button variant="link" asChild>
+            <Link href="/profile">Profile</Link>
+          </Button>
         </DropdownMenuItem>
-        <DropdownMenuItem>
-          <LogoutButton />
-        </DropdownMenuItem>
+
+        {user ? (
+          <DropdownMenuItem>
+            <LogoutButton />
+          </DropdownMenuItem>
+        ) : null}
       </DropdownMenuContent>
     </DropdownMenu>
   );
